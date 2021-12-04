ec63afaaef45c53edcb2126f0ac2b51ef07e7ca9
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-12-04 11:52:42 -0300

    Criado lógica para coletar os dados com sucesso

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
+# Caminho do projeto 
+PROJECT_PATH = 'C:/Users/guisa/Desktop/Processo-Seletivo-Brasil-Capital'
+#TODO Criar spider para o backup constellation: scrapy genspider constellation2 site.com
 # Importações
+from json.decoder import JSONDecodeError
 import os
+import json
+os.system('cls')
 
 # Coletar dados das gestoras
-gestoras = ['nucleo', 'dynamo']
+# gestoras = ['dynamo', 'constellation', 'nucleo']
+gestoras = ['nucleo']
 
 for gestora in gestoras:
     print(f'\nObtendo dados da {gestora}')
     os.chdir(f'web_scraping/{gestora}')
     try:
+        # Tenta adquirir os dados, utilizando o Scrapy, a partir dos sites das gestoras
         os.system(f'scrapy crawl ws{gestora} -O ../rentabilidades_resultados/rentabilidades_{gestora}.json')
-        print('\n\nDados adquiridos com sucesso!')
-    except:
-        raise print('ERRO! Houve algum problema')
+        
+        # Verifica se os arquivos JSON não estão vazios
+        arquivo = open(PROJECT_PATH + f'/web_scraping/rentabilidades_resultados/rentabilidades_{gestora}.json')
+        try: 
+            dados = json.load(arquivo)
+            print('\n\nDados adquiridos com sucesso!')
+        except JSONDecodeError:
+            # Arquivo vazio -> Tenta adquirir dados, utilizando o Scrapy, a partir do site da Anbima
+            print(f'\nErro ao adquirir dados pelo site da {gestora}. Tentando a partir do site da Anbima\n')
 
-    os.chdir('C:/Users/guisa/Desktop/Processo Seletivo Brasil Capital')
+    except:
+        raise print(f'Houve algum problema na coleta a partir do site da {gestora}')
+         
+
+    os.chdir(PROJECT_PATH)
