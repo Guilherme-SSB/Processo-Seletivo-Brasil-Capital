812905b535d4c6829ab2a0ff7b543fd2b1d2bd4d
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-12-26 22:24:00 -0300

    update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Variáveis globais do projeto
 
 ## Caminho do projeto
-PROJECT_PATH = 'C:/Users/guisa/Desktop/Processo-Seletivo-Brasil-Capital'
+PROJECT_PATH = 'C:/Users/guisa/Documents/Github/Processo Seletivo Brasil Capital'
 
 ## Configurações do SQL Server
 dados_conexao = (
